fb1b01190eb4eb08adde35f3e16add9b2b1b2a64
jlk9 <josek97@utexas.edu>
2023-01-18 10:17:23 -0600

    Finished cleaning up functions in run_column_step

--- src/column_physics/run_ice_column.jl
+++ src/column_physics/run_ice_column.jl
@@ -59,17 +59,14 @@ end
     # Computes the surface fluxes at this time step
     step_surface_flux(jcolumn, atmodel, step)
     
-    
     # Computes the temperature changes at this step
-    step_temp_change(jcolumn.F_0[step], jcolumn.dF_0[step], jcolumn)
+    step_temp_change(jcolumn, step)
 
     # Gets the growth/melt and rebalances
-    step_growth_melt(jcolumn.N_i, jcolumn.N_s, jcolumn.H_s, jcolumn.S, jcolumn.T_frz, jcolumn.Δh, jcolumn.T_nplus, jcolumn.K, jcolumn.K̄,
-                     jcolumn.q, jcolumn.q_new, jcolumn.z_old, jcolumn.z_new, jcolumn.Δt, jcolumn.u_star, jcolumn.T_w, jcolumn.F_0[step])
+    step_growth_melt(jcolumn, step)
 
     # Add up the layer thicknesses to get the new total thickness
     readd_total_thickness(jcolumn)
-    
 end
 
 # Julia's sum() operation is memory inefficient for slices of arrays, this is much faster
