40d749359fcdc6777e12874e18417a3c7317b94b
jlk9 <josek97@utexas.edu>
2023-01-25 13:37:37 -0600

    Adding lateral melt function

---
+++ src/grid_cell/lateral_melt.jl
@@ -0,0 +1,12 @@
+# Written by Joseph Kump (josek97@utexas.edu)
+# Code that computes the lateral melt of ice in each grid cell column after remapping and adding of new ice
+
+include("./jicecell_struct.jl")
+
+#=
+=#
+@inline function lateral_melt(jcell)
+
+
+    return nothing
+end
