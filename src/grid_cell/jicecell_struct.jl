41f1a4e4b0c1ff2fcc8ce283b205eb98aba4ee59
jlk9 <josek97@utexas.edu>
2023-01-27 12:09:35 -0600

    Added long overdue documentation for JICEColumn fields

--- src/grid_cell/jicecell_struct.jl
+++ src/grid_cell/jicecell_struct.jl
@@ -12,7 +12,57 @@ const dSin0_frazil     = 3.0     # bulk salinity reduction of newly formed frazi
 const ice_ref_salinity = 4.0     # reference salinity of sea ice, ppt
 
 #= JICECell struct
+Properties:
+    N_cat (dim'less)   Number of thickness categories, int
+    N_t (dim'less) Number of time steps in model, int
 
+    T_frz      (C)          freezing point of water at salinity S, float
+    T_w        (C)          Temperature of water at ocean surface, float
+    frzmlt     (W / m^2)    freezing / melting potential of ice,   float
+    rside      (dim'less)   fraction of ice that metls laterally,  float
+    fresh_flux ()           freshwater flux to the ocean, float
+    salt_flux  ()           saltwater flux to the ocean,  float
+    heat_flux  ()           heat flux to the ocean,       float
+
+    H_bds       (m)         upper and lower thickness bounds for each thickness category,                            Vector{Float64} of length N_cat+1
+    H_bnew      (m)         upper and lower thickness bounds for temporary categories for linear remapping,          Vector{Float64} of length N_cat+1
+    dH          (m)         amount the ice column height H_i changed in each category after vertical thermodynamics, Vector{Float64} of length N_cat
+    areas       (dim'less)  percent area of the gridcell covered by each thickness category,                         Vector{Float64} of length N_cat
+    vol_i       (m^3)       volume of ice in each thickness category,                                                Vector{Float64} of length N_cat
+    vol_s       (m^3)       volume of snow in each thickness category,                                               Vector{Float64} of length N_cat
+    areas_old   (dim'less)  old areas for conservation computations,                                                 Vector{Float64} of length N_cat
+    vol_i_old   (m^3)       old ice volumes for conservation computations,                                           Vector{Float64} of length N_cat
+    vol_s_old   (m^3)       old snow volumes for conservation computations,                                          Vector{Float64} of length N_cat
+    columns     (a lot)     the ice columns that represent each thickness category,                                  Vector{JICEColumn} of length N_cat
+
+    (NOTE: total area is in m^2 but it is fixed for the grid cell)
+
+    # Variables for the fit_line helper in linear remapping, using a linear approximation of the thickness distribution function
+    g0 () intercept of linear approximation, Vector{Float64}
+    g1 () slope of linear approximation,     Vector{Float64}
+    hL () new category lower bound,          Vector{Float64}
+    hR () new category upper bound,          Vector{Float64}
+
+    dareas (dim'less)   change in area after linear remapping, Vector{Float64}
+    dvol_i (m^3)        change in ice volume after linear remapping, Vector{Float64}
+    dvol_s (m^3)        change in snow volume after linear remapping, Vector{Float64}
+    donor  (dim'less)   source of new ice/snow for each column after remapping, Vector{Int}
+
+
+    i_energy     (E)    ice energy after grid cell process (linear remapping, adding new ice),   Vector{Float64}
+    s_energy     (E)    snow energy after grid cell process (linear remapping),                  Vector{Float64}
+    i_energy_old (E)    ice energy before grid cell process (linear remapping, adding new ice),  Vector{Float64}
+    s_energy_old (E)    snow energy before grid cell process (linear remapping),                 Vector{Float64}
+
+    d_area_i_new (dim'less)     ice area from new growth to add to each thickness category, Vector{Float64}
+    d_area_total (dim'less)     total ice area to add to each thickness category, Vector{Float64}
+
+    (NOTE: for now d_area_total = d_area_i_new, but if fsd is included then it will also include lateral growth)
+
+    vi0_new_array (m^3)         volume of new ice to add into each category in add_new_ice,             Vector{Float64}
+    rside_array   (dim'less)    fraction of ice melted from each thickness category at each time step,  Vector{Float64}
+
+    atm (a lot)     accompanying atmosphere "model" with relevant atmosphere variables, ATModel
 =#
 mutable struct JICECell
 
