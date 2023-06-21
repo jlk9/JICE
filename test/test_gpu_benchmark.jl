37377ced32a58922241c6982b0c0c33a2fd8e3c9
Joseph Kump <jlk3675@login1.ls6.tacc.utexas.edu>
2023-06-21 11:58:42 -0500

    fixed typo in test setup

--- test/test_gpu_benchmark.jl
+++ test/test_gpu_benchmark.jl
@@ -96,8 +96,8 @@ CUDA.@time run_ice_column(jarrays, atmodels)
 
 println("Now for the CPU comparison:")
 onDevice = false
-@btime initialize_ATModelArrays(N_t, N_c, F_SWvdr, F_SWidr, F_SWvdf, F_SWidf, F_Ld, T_a, Θ_a, ρ_a, Q_a, c_p, U_a, onDevice)
-@btime initialize_JICEColumnArrays(N_t, N_c, N_i, N_s, H_i, H_s, T_frz, Δt, u_star, T_w, T_0, onDevice)
+@btime atmodels = initialize_ATModelArrays(N_t, N_c, F_SWvdr, F_SWidr, F_SWvdf, F_SWidf, F_Ld, T_a, Θ_a, ρ_a, Q_a, c_p, U_a, onDevice)
+@btime jarrays  = initialize_JICEColumnArrays(N_t, N_c, N_i, N_s, H_i, H_s, T_frz, Δt, u_star, T_w, T_0, onDevice)
 @btime run_ice_column(jarrays, atmodels)
 
 
@@ -139,8 +139,8 @@ CUDA.@time run_ice_column(jarrays, atmodels)
 
 println("Now for the CPU comparison:")
 onDevice = false
-@btime initialize_ATModelArrays(N_t, N_c, F_SWvdr, F_SWidr, F_SWvdf, F_SWidf, F_Ld, T_a, Θ_a, ρ_a, Q_a, c_p, U_a, onDevice)
-@btime initialize_JICEColumnArrays(N_t, N_c, N_i, N_s, H_i, H_s, T_frz, Δt, u_star, T_w, T_0, onDevice)
+@btime atmodels = initialize_ATModelArrays(N_t, N_c, F_SWvdr, F_SWidr, F_SWvdf, F_SWidf, F_Ld, T_a, Θ_a, ρ_a, Q_a, c_p, U_a, onDevice)
+@btime jarrays  = initialize_JICEColumnArrays(N_t, N_c, N_i, N_s, H_i, H_s, T_frz, Δt, u_star, T_w, T_0, onDevice)
 @btime run_ice_column(jarrays, atmodels)
 
 
